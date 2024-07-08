1f51fd59ef3e6892a4c65d715befa9ce3f94f656
Shubham <sksingh94166@gmail.com>
2024-07-08 16:44:40 +0530

    Update Create and Mint Token.sol

--- Create and Mint Token.sol
+++ Create and Mint Token.sol
@@ -1,63 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract SimpleToken {
-    string public name = "SimpleToken";
-    string public symbol = "STK";
-    uint8 public decimals = 18;
-    uint256 public totalSupply;
-    address public owner;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 
-    mapping(address => uint256) public balances;
-    mapping(address => mapping(address => uint256)) public allowances;
+contract MyToken is ERC20, Ownable, Pausable {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) Ownable(msg.sender) {}
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event TokensMinted(address indexed to, uint256 amount);
+    event TokensBurned(address indexed from, uint256 amount);
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Not the contract owner");
-        _;
+    function mint(address to, uint256 amount) external onlyOwner whenNotPaused {
+        _mint(to, amount);
+        emit TokensMinted(to, amount);
     }
 
-    constructor() {
-        owner = msg.sender;
+    function burn(uint256 amount) external whenNotPaused {
+        _burn(msg.sender, amount);
+        emit TokensBurned(msg.sender, amount);
     }
 
-    function transfer(address to, uint256 amount) public returns (bool) {
-        require(balances[msg.sender] >= amount, "Insufficient balance");
-        balances[msg.sender] -= amount;
-        balances[to] += amount;
-        emit Transfer(msg.sender, to, amount);
-        return true;
+    function pause() external onlyOwner {
+        _pause();
     }
 
-    function approve(address spender, uint256 amount) public returns (bool) {
-        allowances[msg.sender][spender] = amount;
-        emit Approval(msg.sender, spender, amount);
-        return true;
+    function unpause() external onlyOwner {
+        _unpause();
     }
 
-    function transferFrom(address from, address to, uint256 amount) public returns (bool) {
-        require(balances[from] >= amount, "Insufficient balance");
-        require(allowances[from][msg.sender] >= amount, "Allowance exceeded");
-        balances[from] -= amount;
-        balances[to] += amount;
-        allowances[from][msg.sender] -= amount;
-        emit Transfer(from, to, amount);
-        return true;
+    function transfer(address recipient, uint256 amount) public override whenNotPaused returns (bool) {
+        return super.transfer(recipient, amount);
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        totalSupply += amount;
-        balances[to] += amount;
-        emit Transfer(address(0), to, amount);
-    }
-
-    function burn(uint256 amount) public {
-        require(balances[msg.sender] >= amount, "Insufficient balance");
-        totalSupply -= amount;
-        balances[msg.sender] -= amount;
-        emit Transfer(msg.sender, address(0), amount);
+    function transferFrom(address sender, address recipient, uint256 amount) public override whenNotPaused returns (bool) {
+        return super.transferFrom(sender, recipient, amount);
     }
 }
-
